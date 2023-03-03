c0d2f9c11065a70fdb76847a30ed69b4764a5630
tmddk9445 <tmddk9445@naver.com>
2023-03-03 17:44:56 +0900

    2023.03.03 springboot Jpa

--- src/main/java/com/example/vsc_springboot/service/MainService.java
+++ src/main/java/com/example/vsc_springboot/service/MainService.java
@@ -1,5 +1,7 @@
 package com.example.vsc_springboot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -79,7 +81,27 @@ public class MainService {
     public void descriptionJpaMethod() {
         //# JpaRepository 기본 메서드
         //! findById().get();
-        //? 
+        //? 해당 테이블에서 PK를 기준으로 값을 검색
+        ExampleEntity exampleEntity = exampleRepository.findById(0).get();
+
+        //! findAll();
+        //? 해당 테이블의 모든 레코드를 검색한 결과를 반환
+        List<ExampleEntity> entityList = exampleRepository.findAll();
+
+        //! savd(entityInstance);
+        //? 해당 테이블의 특정 레코드를 삽입 혹은 수정
+        //? Primary Key를 기준으로 Primary Key에 해당하는 레코드가 없으면 '삽입'
+        //? Primary Key에 해당하는 레코드가 있으면 해당 레코드를 '수정'
+        exampleRepository.save(exampleEntity);
+        
+        //! existsById(PK);
+        //? 해당 테이블에 PK를 기준으로 레코드가 존재한다면 true 반환
+        //? 존재하지 않는다면 false 반환
+        boolean hasEntity = exampleRepository.existsById(0); 
+
+        //! deleteById(PK);
+        //? 해당 테이블에 PK를 기준으로 특정 레코드를 삭제
+        exampleRepository.deleteById(0);
     }
 
 }
