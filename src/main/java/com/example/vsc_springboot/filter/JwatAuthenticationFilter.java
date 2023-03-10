6eab49f9f57e57b85420bf61cd5bab6a7d84fa52
tmddk9445 <tmddk9445@naver.com>
2023-03-10 15:47:09 +0900

    20230310 FRI Springboot

--- src/main/java/com/example/vsc_springboot/filter/JwatAuthenticationFilter.java
+++ src/main/java/com/example/vsc_springboot/filter/JwatAuthenticationFilter.java
@@ -7,35 +7,74 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.example.vsc_springboot.provider.TokenProvider;
+
 @Component
 public class JwatAuthenticationFilter extends OncePerRequestFilter{
+
+  @Autowired private TokenProvider tokenProvider;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-      
-        try {
 
-          //? Request Header에 있는 Bearer Token을 가져옴
+    try {
 
+      // ? Request Header에 있는 Bearer Token을 가져옴
+      String token = parseBearerToken(request);
 
-        } catch (Exception exception) {
-          exception.printStackTrace();
-        }
+      // ? token이 있는지
+      if (token == null) {
+
+        String sub = tokenProvider.validate(token);
+
+        AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(sub, null,
+            AuthorityUtils.NO_AUTHORITIES);
+
+        authenticationToken
+            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authenticationToken);
+        SecurityContextHolder.setContext(securityContext);
+
+      }
+
+    } catch (Exception exception) {
+      exception.printStackTrace();
+    }
+
+    filterChain.doFilter(request, response);
+
   }
 
   private String parseBearerToken(HttpServletRequest request) {
     //? Request Header의 Authorization 필드의 Value를 가져옴
     String authorizationValue = request.getHeader("Authorization");
 
-    if (StringUtils.hasText(authorizationValue) && authorizationValue.startsWith("Bearer ")) 
-      return authorizationValue.substring(7);
+    //? Authorization Value에 문자가 포함되어 있는지
+    boolean hasTokenValue = StringUtils.hasText(authorizationValue);
+    if (!hasTokenValue) return null;
 
-    return null;
+    //? Authorization Value가 Bearer로 시작되는지
+    boolean isBearer = authorizationValue.startsWith("Bearer ");
+    if (!isBearer) return null;
+
+    //? "Bearer " 다음에 오는 문자열(Token)을 추출
+    String token = authorizationValue.substring(7);
+    return token;
+
   }
   
 }
